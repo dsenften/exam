181cdce7727f0b6805e17a6ded97e394074914be
Arugan A <arugan10@icloud.com>
2019-03-19 13:30:43 +0100

    Ich bin fertig

--- src/main/java/inheritance/Car.java
+++ src/main/java/inheritance/Car.java
@@ -4,6 +4,7 @@ package inheritance;
 public class Car {
 
     private CarType type;
+    private double price;
 
     public Car(CarType type) {
         this.type = type;
@@ -11,5 +12,13 @@ public class Car {
 
     public CarType getType() {
         return type;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
     }
 }
